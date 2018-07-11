ceb64e628c8c9d13343ec754c34c1789a36cc65b
woolsox <dimeo.colin@gmail.com>
2018-07-10 21:30:06 -0400

    adds sprite flip

--- d_d/PlayerController.cs
+++ d_d/PlayerController.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float speed;
     public float jumpForce;
-    private float moveInput;
+    float moveInput;
+    bool facingRight = true;
+     
+    Rigidbody2D rb;
 
-    private Rigidbody2D rb;
-
-    private void Start()
-    {
+    void Start(){
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
-    {
+    void FixedUpdate(){
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0){
+            Flip();
+        } else if(facingRight == true && moveInput < 0) {
+            Flip();
+        }
+    }
+
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }
