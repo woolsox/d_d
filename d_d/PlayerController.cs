e66bded81c254118423615e07131cfe081bef7b2
woolsox <dimeo.colin@gmail.com>
2018-07-10 22:24:59 -0400

    adds frictionless wall contact, jumping and a ground collision check

--- d_d/PlayerController.cs
+++ d_d/PlayerController.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float speed;
     public float jumpForce;
+
     float moveInput;
     bool facingRight = true;
-     
+
+    bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
     Rigidbody2D rb;
 
-    void Start(){
+    int extraJumps;
+    public int extraJumpsValue;
+
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position,
+                                                         checkRadius,
+                                                         whatIsGround);
+
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
@@ -25,7 +41,21 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    void Flip(){
+    void Update()
+    {
+        if(isGrounded == true) {
+            extraJumps = 2;
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0){
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
         facingRight = !facingRight;
         Vector3 Scaler = transform.localScale;
         Scaler.x *= -1;
